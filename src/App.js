af1ba359c45633a43382c60f9dae22f8d6142dd1
UFFETKK <soyoungan02@naver.com>
2023-11-05 20:05:24 +0900

    feat : 로또의 구입 금액을 입력받는다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const UNIT = 1000;
 class App {
-  async play() {}
+  async play() {
+    this.numberOfPurchases = await this.setPurchaseAmount();
+    Console.print(`\n${this.numberOfPurchases}개를 구매했습니다.`);
+  }
+
+  async setPurchaseAmount() {
+    const purchaseAmount = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+    if (purchaseAmount % UNIT) throw new Error("1000 단위로 입력해주세요.");
+
+    return parseInt(purchaseAmount / UNIT);
+  }
 }
 
 export default App;
